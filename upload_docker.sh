b8882b75ecd97645d2fcf97d9b666cb4d1f69e7a
Donko Dimov <donko@abv.bg>
2021-01-19 21:12:24 +0100

    refactor: Accept tag as user input

--- upload_docker.sh
+++ upload_docker.sh
@@ -7,14 +7,15 @@
 # Create dockerpath
 
 dockerpath=donko/btcroc
+read tag
 
 # Step 2:  
 # Authenticate & tag
 docker login --username=donko
-docker tag btcroc ${dockerpath}
+docker tag btcroc ${dockerpath}$tag
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
 
-docker push ${dockerpath}
+docker push ${dockerpath}:$tag
